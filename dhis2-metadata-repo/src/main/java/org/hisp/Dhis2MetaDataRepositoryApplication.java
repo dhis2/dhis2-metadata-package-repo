78cc01712dc7cda840214ac5769c84bb232e8d8e
zubaira <zubair@dhis2.org>
2017-01-31 18:42:56 +0100

    Initial Commit

---
+++ dhis2-metadata-repo/src/main/java/org/hisp/Dhis2MetaDataRepositoryApplication.java
@@ -0,0 +1,12 @@
+package org.hisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dhis2MetaDataRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dhis2MetaDataRepositoryApplication.class, args);
+	}
+}
