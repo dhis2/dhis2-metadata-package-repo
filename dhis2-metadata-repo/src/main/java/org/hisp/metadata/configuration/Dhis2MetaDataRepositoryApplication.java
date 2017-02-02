1da65850651bef5b4c27b599ab2ddf71c313d438
zubaira <zubair@dhis2.org>
2017-02-02 23:58:26 +0100

    updated pom

---
+++ dhis2-metadata-repo/src/main/java/org/hisp/metadata/configuration/Dhis2MetaDataRepositoryApplication.java
@@ -0,0 +1,13 @@
+package org.hisp.metadata.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dhis2MetaDataRepositoryApplication
+{
+	public static void main( String[] args )
+	{
+		SpringApplication.run( Dhis2MetaDataRepositoryApplication.class, args );
+	}
+}
